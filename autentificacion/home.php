900b7434868caa3cfd950f3d76a82dab1fc4bc0c
Andrea9205 <acbautistap@utn.edu.ec>
2017-01-13 12:19:51 -0500

    Autentificacion

---
+++ autentificacion/home.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+
+?>
+<html>
+	<head>
+		<title>.: HOME :.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+		<h2>Bienvenido a Optativa II</h2>
+
+
+</div>
+</div>
+</div>
+	</body>
+</html>
---
+++ autentificacion/php/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$host="http://carina.jl.serv.net.mx/";
+$user="root";
+$password="FHOrfk11241";
+$db="autentificacion";
+$con = new mysqli($host,$user,$password,$db);
+
+?>
